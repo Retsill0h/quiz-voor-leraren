a75bc44999839073737749a826daca76bfd7f973
Retsilloh <ptran058@gmail.com>
2015-06-03 10:47:56 +0200

    .

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,9 +1,13 @@
 class Admin::QuestionsController < ApplicationController
 	def index
 		@question = Question.all
+		@question.build_answer
 	end
 	def new
 		@question = Question.new
 		@chapter = Chapter.all
+	end
+	def create
+
 	end
 end
