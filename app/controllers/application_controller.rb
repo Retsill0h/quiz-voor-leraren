8e98699d0c5a49049baae4cfe926075d48f31949
Retsilloh <ptran058@gmail.com>
2015-06-23 16:30:30 +0200

    Add random and fix admin password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
+	protect_from_forgery with: :exception
+	protected
+		def admin_required
+		authenticate_or_request_with_http_basic do |username, password|
+		username == "user" && password == "user"
+		end
+	end
 end
